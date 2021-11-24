d3e36eb6fb9d332278542e9d8bc18c00904f8e3c
Daniel Monaghan <mowglixx@users.noreply.github.com>
2021-11-24 22:15:15 +0000

    Update chal3.py

--- Day3/chal3.py
+++ Day3/chal3.py
@@ -1,10 +1,19 @@
+#
+# Challenge 3
+#
+# Create a variable called num.
+# If num is divisible by 3 print “fizz”,
+# if it’s divisible by 7 print “buzz”,
+# if it’s divisible by both 3 and 7 print
+# “fizz buzz”.
+# Otherwise print num.
+
 def fizzbuzz():
     # cast num into int
     num = int(input('enter a number: '))
-    # setup
 
+    # create a function to check the divisable number and return a boolean for use in if statments
     def is_div(div):
-        # import floor for handeling floats
         from math import floor
         if floor(num % div) == 0:
             return True
@@ -19,5 +28,5 @@ def fizzbuzz():
             print('buzz')
         else:
             print(num)
-    print('Divisible by 3?', is_div(3))
-    print('Divisible by 7?', is_div(7))
+    print(f'{num} Divisible by 3: {is_div(3)}')
+    print(f'{num} Divisible by 7: {is_div(7)}')
