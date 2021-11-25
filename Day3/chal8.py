3fbe4b199b87df32ac1dbe624fa44b9d3352918d
Daniel Monaghan <mowglixx@users.noreply.github.com>
2021-11-25 00:05:23 +0000

    Completed Challenge 8

--- Day3/chal8.py
+++ Day3/chal8.py
@@ -1,15 +1,23 @@
+#
+# Challenge 8 (Extra)
+#
 # Take the string
 # 'jrfndklhgfndjkjlkgperfijfhdknsadcvjhiiohjfkledsopiuhgtyujwsdxcvhgfdjhiopiwquhejkdsoiufghedjwshi'.
 # Find the index of a last vowel in the string.
 
 
 def last_vowel():
-    query_string = 'jrfndklhgfndjkjlkgperfijfhdknsadcvjhiiohjfkledsopiuhgtyujwsdxcvhgfdjhiopiwquhejkdsoiufghedjwshi'
-    query_string_reversed = query_string[::-1]
-    vowels = ['a', 'e', 'i', 'o', 'u']
+    big_string = 'jrfndklhgfndjkjlkgperfijfhdknsadcvjhiiohjfkledsopiuhgtyujwsdxcvhgfdjhiopiwquhejkdsoiufghedjwshi'
+    VOWELS = ['a', 'e', 'i', 'o', 'u']
 
-    # print(letter)
+    # enumerate the string until no more matches
+    for letter_index, letter in enumerate(big_string):
+        if letter in VOWELS:
+            # update the values when a match is found until no more matches
+            last_vowel_found = letter
+            last_vowel_found_index = letter_index
 
-    for vowel in vowels:
-        print(query_string_reversed.index(vowel))
-        break
+    print(f'last_vowel: {last_vowel_found}')
+    print(f'last_vowel_index: {last_vowel_found_index}')
+    print(
+        f'`big_string[{last_vowel_found_index}]` returns: {big_string[last_vowel_found_index]}')
